4ac49d740c43dd60fba41667be7510dc4f44d2c2
Bae-jihyun <govl6113@gmail.com>
2022-05-02 21:39:25 +0900

    feat: eureka client 등록

--- src/main/kotlin/kr/soupio/soup/CeSoupServerApplication.kt
+++ src/main/kotlin/kr/soupio/soup/CeSoupServerApplication.kt
@@ -2,8 +2,13 @@ package kr.soupio.soup
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableJpaAuditing
 class CeSoupServerApplication
 
 fun main(args: Array<String>) {
